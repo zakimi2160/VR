45585b9fa91260adc91a7cd41e4d441a47602725
zakimi2160 <zakimi2160@gmail.com>
2016-06-18 01:13:38 +0900

    candy
    aaa

---
+++ CandyDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyDestroyer : MonoBehaviour {
+    public GameHolder candyHolder;
+    public int reward;
+
+    public GameObject effectPrefab;//エフェクトプリファブ
+    public Vector3 effectRotation;//エフェクトの位置
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Candy")
+        {
+            candyHolder.AddCandy(reward);
+            //オブジェクト削除
+            Destroy(other.gameObject);
+
+            if (effectPrefab != null)
+            {
+                //Candyのポジションにエフェクト発生
+                Instantiate(effectPrefab, other.transform.position, Quaternion.Euler(effectRotation));
+               
+            }
+        }
+
+    }
+}
